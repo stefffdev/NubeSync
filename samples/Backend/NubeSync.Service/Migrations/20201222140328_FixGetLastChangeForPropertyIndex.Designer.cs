a8fac20601e82200c13d8d62d87b162123db7474
stefffdev <stefff@live.at>
2020-12-22 15:03:56 +0100

    fixed index for gestlastchangeforproperty

--- samples/Backend/NubeSync.Service/Migrations/20201222140328_FixGetLastChangeForPropertyIndex.Designer.cs
+++ samples/Backend/NubeSync.Service/Migrations/20201222140328_FixGetLastChangeForPropertyIndex.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NubeSync.Service.Data;
 
 namespace NubeSync.Service.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20201222140328_FixGetLastChangeForPropertyIndex")]
+    partial class FixGetLastChangeForPropertyIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
