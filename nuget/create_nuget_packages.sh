204d256cea87436ae4b1a9ec3d6be6f5bc1cca6b
Stefan Seethaler <stefan.seethaler@lakedice.com>
2021-06-15 19:42:44 +0200

    updated build scripts

--- nuget/create_nuget_packages.sh
+++ nuget/create_nuget_packages.sh
@@ -7,5 +7,3 @@ yes | cp -rf ../src/NubeSync.Client/bin/Release/*nupkg ../publish
 yes | cp -rf ../src/NubeSync.Client.SQLiteStore/bin/Release/*nupkg ../publish
 yes | cp -rf ../src/NubeSync.Core/bin/Release/*nupkg ../publish
 yes | cp -rf ../src/NubeSync.Server/bin/Release/*nupkg ../publish
-
-pause
